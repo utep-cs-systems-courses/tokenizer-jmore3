8dba16ce2b2e04e8b15638e0c8a74ddefa9ccb71
jmore3 <84981020+jmore3@users.noreply.github.com>
2023-10-13 13:04:27 -0600

    Update uimain.c
    added comments

--- src/uimain.c
+++ src/uimain.c
@@ -1,50 +1,58 @@
-#include "stdio.h"
-#include "stdlib.h"
-#include "tokenizer.h"
-#include "history.h"
+#include "stdio.h"          // Include the standard input/output library
+#include "stdlib.h"         // Include the standard library
+#include "tokenizer.h"     // Include your custom "tokenizer" library (assuming it contains necessary data structures)
+#include "history.h"        // Include your custom "history" library (assuming it contains necessary data structures)
 
 int main()
 {
-  int exit = 1;
-  List *list = init_history();
+  int exit = 1;            // Initialize a variable to control the main loop
+  List *list = init_history();  // Create a history list using your "init_history" function
+
   do{
-    printf("Make your selection\n 1. Tokenize 2.Print History 3. Get History 4. Quit (Enter 1, 2, 3 or 4)\n");
+    // Display a menu for user selection
+    printf("Make your selection\n 1. Tokenize 2. Print History 3. Get History 4. Quit (Enter 1, 2, 3, or 4)\n");
     char sel[10];
     printf("> ");
     fgets(sel, 10, stdin);
     char *end;
-    int x = strtol(sel, &end, 10);
+    int x = strtol(sel, &end, 10);  // Convert the user's input to an integer
+
     switch(x){
-    case 1:{
-      car *tokens = (char*)malloc(30*sizeof(char));
-      printf("> ");
-      fgets(tokens, 30, stdin);
-      char **t = tokenize(tokens);
-      add_history(list, tokens);
-      print_tokens(t);
-      free_tokens(t);
-    } break;
-    case 2:{
-      print_history(list);
-      break;
-    }
-    case 3:{
-      char id[10];
-      printf("Enter ID:\n> ");
-      fgets(id, 10, stdin);
-      int i = strtol(id, &end, 10);
-      char *h = get_history(list, i);
-      printf("%d. %s\n", i, h);
-      break;
-    }
-    case 4:{
-      exit = 0;
-      break;
+      case 1:{
+        char *tokens = (char*)malloc(30 * sizeof(char));  // Allocate memory for input tokens
+        printf("> ");
+        fgets(tokens, 30, stdin);
+        char **t = tokenizer(tokens);  // Tokenize the input and get an array of tokens
+        add_history(list, tokens);   // Add the input to the history list
+        print_tokens(t);  // Print the tokens
+        free_tokens(t);   // Free the memory used by the token array
+      } break;
+
+      case 2:{
+        print_history(list);  // Print the history list
+        break;
+      }
+
+      case 3:{
+        char id[10];
+        printf("Enter ID:\n> ");
+        fgets(id, 10, stdin);
+        int i = strtol(id, &end, 10);  // Convert the user's input to an integer
+        char *h = get_history(list, i);  // Get the history item with the specified ID
+        printf("%d. %s\n", i, h);  // Print the history item
+        break;
+      }
+
+      case 4:{
+        exit = 0;  // Set the exit flag to exit the main loop
+        break;
+      }
+
       default:
-	break;
+        break;
     }
-      
-    }while(exit);
-    free_history;
-    return 0;
+  } while (exit);  // Continue the loop while the exit flag is not zero
+
+  free_history(list);  // Free the memory used by the history list
+  return 0;
 }
